22315612ee10a5d704bee927e18612edb3c147f8
voronovmaksim88 <voronovmaksim88@gmail.com>
2024-09-14 13:39:13 +0700

    С большим трудом за 2 часа добавил кое-как poetry

---
+++ beckend/beckend/main.py
@@ -0,0 +1,45 @@
+import asyncio
+
+from fastapi import FastAPI, HTTPException, Body
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from fastapi import Depends
+from starlette.responses import HTMLResponse, FileResponse
+from pydantic import BaseModel, Field
+
+import uvicorn
+
+#
+# from database import async_session_maker, test_connection
+# from sqlalchemy.ext.asyncio import AsyncSession
+# from sqlalchemy import select
+
+# from test_views import router as test_router
+# from models.models import Country, Manufacturer
+
+
+app = FastAPI()
+# app.include_router(test_router)  # Добавляем роутер для тестовых запросов
+
+# Настройка CORS
+app.add_middleware(
+    CORSMiddleware,  # type: ignore
+    allow_origins=["*"],  # Разрешить все источники
+    allow_credentials=True,
+    allow_methods=["*"],  # Разрешить все методы
+    allow_headers=["*"],  # Разрешить все заголовки
+)
+
+# для автоматического перезапуска приложения при изменении кода
+if __name__ == "__main__":
+    # uvicorn.run("main:app", reload=True)
+    # asyncio.run(test_connection())
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
+
+@app.get("/")
+def home():
+    """Домашняя страница"""
+    html_content = "<h2>FastAPI is the best backend framework</h2>"
+    html_content += '<p>Интерактивная документация на <a href="/docs">/docs</a></p>'
+    return HTMLResponse(content=html_content)
