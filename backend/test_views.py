8ad7e6a5e8a18c24df02ff0f74689e0d6f63ec45
Maksim <voronovmaksim88@gmail.com>
2024-09-18 17:56:15 +0700

    чутка перетосовал тестовые функции

--- backend/test_views.py
+++ backend/test_views.py
@@ -11,6 +11,10 @@ router = APIRouter(prefix="/test")
 def hello_world():
     return {"message": "Hello World!!"}
 
+@router.get("/hello")
+def hello_name(name: str):
+    user_name = name.strip().title()
+    return {"message": f"Hello {user_name}!"}
 
 # Пример данных пользователей
 users = [
@@ -21,7 +25,6 @@ users = [
     {'id': 5, 'name': "Petr", 'age': 50},
 ]
 
-
 @router.get("/user/{user_id}")
 def get_user(user_id: int):
     user = next((user for user in users if user.get('id') == user_id), None)
@@ -29,40 +32,15 @@ def get_user(user_id: int):
         raise HTTPException(status_code=404, detail="User not found")
     return JSONResponse(content=user)
 
-
-# Пример данных заказов
-orders = [
-    {'id': 1, 'name': "order1", 'priority': 5},
-    {'id': 2, 'name': "order2", 'priority': 4},
-    {'id': 3, 'name': "order3", 'priority': 3},
-    {'id': 4, 'name': "order4", 'priority': 2},
-    {'id': 5, 'name': "order5", 'priority': 1},
-    {'id': 6, 'name': "order6", 'priority': 1},
-]
-
-
-@router.get("/order")
-def get_orders(offset: int = 0, limit: int = 10):  # Увеличен по умолчанию limit до 10
-    return orders[offset:offset + limit]
-
-
-@router.get("/hello")
-def hello_name(name: str):
-    user_name = name.strip().title()
-    return {"message": f"Hello {user_name}!"}
-
-
 @router.get("/load_test_html_page")
 def root():
     return FileResponse("content/HTML_example.html")
-
 
 @router.get("/load_test_file")
 def root():
     return FileResponse("content/test_file.txt",
                         filename="test_file.txt",
                         media_type="application/octet-stream")
-
 
 @router.post("/summa")
 def summa(data=Body()):
@@ -79,3 +57,20 @@ class Multiplication(BaseModel):
 @router.post("/mult")
 def hello(multipliers: Multiplication):
     return {"message": f"произведение = {multipliers.m1 * multipliers.m2}"}
+
+
+# Пример данных заказов
+orders = [
+    {'id': 1, 'name': "order1", 'priority': 5},
+    {'id': 2, 'name': "order2", 'priority': 4},
+    {'id': 3, 'name': "order3", 'priority': 3},
+    {'id': 4, 'name': "order4", 'priority': 2},
+    {'id': 5, 'name': "order5", 'priority': 1},
+    {'id': 6, 'name': "order6", 'priority': 1},
+]
+
+@router.get("/order")
+def get_orders(offset: int = 0, limit: int = 10):  # Увеличен по умолчанию limit до 10
+    return orders[offset:offset + limit]
+
+
