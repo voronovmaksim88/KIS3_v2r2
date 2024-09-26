a7b04a6913b658900e54258b241a938368aea9ed
Maksim <voronovmaksim88@gmail.com>
2024-09-26 13:50:39 +0700

    Вынес получение списка производителей в отдельное приложение

--- frontend/src/components/TestDataBase/The_Test_DB_manufacturers.vue
+++ frontend/src/components/TestDataBase/The_Test_DB_manufacturers.vue
@@ -8,25 +8,33 @@ const props = defineProps({
   }
 });
 
-// функция получает список всех стран из БД PostgreSQL
-const all_countries = ref("") // список всех стран
+// функция получает список всех производителей из БД PostgreSQL
+const all_manufacturers = ref("") // список всех стран
 const connection_error = ref("") // список всех стран
-async function fetchCountry() {
+async function fetchManufacturers() {
   connection_error.value = '';
-  all_countries.value = ""; // изначально обнуляем список стран
+  all_manufacturers.value = ""; // изначально обнуляем список стран
   const timeout = 3000; // 3 секунды
-  const fetchPromise = fetch(`${props.url}all_countries`);
+  const fetchPromise = fetch(`${props.url}all_manufacturers`);
   const timeoutPromise = new Promise((_, reject) =>
       setTimeout(() => reject(new Error('Request timed out')), timeout)
   );
 
+  /**
+   * @typedef {Object} ServerResponse
+   * @property {string[]} manufacturers
+   */
+
+  /**
+   * @param {ServerResponse} data
+   */
   try {
     const response = await Promise.race([fetchPromise, timeoutPromise]);
     console.log('Response status:', response.status);
     if (response.ok) {
       const data = await response.json();
       console.log('Received data:', data);
-      all_countries.value = data.countries;
+      all_manufacturers.value = data.manufacturers;
       connection_error.value = ""; // если получен ответ скидываем ошибку
     } else {
       console.log('Response not OK:', response);
@@ -37,35 +45,39 @@ async function fetchCountry() {
     } else {
       console.error('Error fetching countries:', error);
     }
-    connection_error.value = error.message ;
-    all_countries.value = "";
+    connection_error.value = error.message;
+    all_manufacturers.value = "";
   }
 }
 </script>
 
 <template>
   <div class="grid grid-cols-4 gap-2">
+
     <div class="col-span-4">
-      <button class="btn btn-p " @click="fetchCountry">Get country from DB</button>
+      <button class="btn btn-p " @click="fetchManufacturers">Get Manufacturers from DB</button>
     </div>
-    <div class="col-span-4" v-if="all_countries && all_countries.length > 0">
-      <table class="text-white w-full">
+
+    <div class="col-span-4" v-if="all_manufacturers && all_manufacturers.length > 0">
+      <table>
         <thead>
         <tr>
           <th>id</th>
           <th>name</th>
+          <th>country_id</th>
         </tr>
         </thead>
-        <tbody>
-        <tr v-for="country in all_countries" :key="country.id">
-          <td>{{ country.id }}</td>
-          <td>{{ country.name }}</td>
+        <tr v-for="manufacturer in all_manufacturers" :key="manufacturer.id">
+          <td>{{ manufacturer.id }}</td>
+          <td>{{ manufacturer.name }}</td>
+          <td v-if="'country_id' in manufacturer">{{ manufacturer.country_id }}</td>
+          <td v-else>N/A</td>
         </tr>
-        </tbody>
       </table>
     </div>
     <p class="text-red-400 col-span-4" v-if="connection_error">{{ connection_error }}</p>
-    <hr class="col-span-4 mb-8">
+    <hr class="col-span-4 mb-8"> <!-- Обратите внимание, что мы добавили col-span-4 для лучшего управления отступом -->
+
   </div>
 </template>
 
--- frontend/src/components/TestDataBase/The_Test_Data_Base.vue
+++ frontend/src/components/TestDataBase/The_Test_Data_Base.vue
@@ -1,52 +1,13 @@
 <script setup>
-import {ref} from 'vue'
+
 import The_Test_DB_countries from "./The_Test_DB_countries.vue";
+import The_Test_DB_manufacturers from "./The_Test_DB_manufacturers.vue";
 
 // const backend_url = "https://sibplc-kis3.ru/api/"
 // const backend_url = "http://localhost:8000/api/"
 const backend_url = import.meta.env.VITE_API_URL; // Если используете Vite
 
 
-// функция получает список всех производителей из БД PostgreSQL
-const all_manufacturers = ref("") // список всех стран
-const all_manufacturers_error = ref("") // список всех стран
-async function fetchManufacturers() {
-  all_manufacturers.value = ""; // изначально обнуляем список стран
-  const timeout = 3000; // 3 секунды
-  const fetchPromise = fetch(`${backend_url}all_manufacturers`);
-  const timeoutPromise = new Promise((_, reject) =>
-      setTimeout(() => reject(new Error('Request timed out')), timeout)
-  );
-
-  /**
-   * @typedef {Object} ServerResponse
-   * @property {string[]} manufacturers
-   */
-
-  /**
-   * @param {ServerResponse} data
-   */
-  try {
-    const response = await Promise.race([fetchPromise, timeoutPromise]);
-    console.log('Response status:', response.status);
-    if (response.ok) {
-      const data = await response.json();
-      console.log('Received data:', data);
-      all_manufacturers.value = data.manufacturers;
-      all_manufacturers_error.value = ""; // если получен ответ скидываем ошибку
-    } else {
-      console.log('Response not OK:', response);
-    }
-  } catch (error) {
-    if (error.message === 'Request timed out') {
-      console.error('Request timed out after', timeout, 'ms');
-    } else {
-      console.error('Error fetching countries:', error);
-    }
-    all_manufacturers_error.value = error;
-    all_manufacturers.value = "";
-  }
-}
 </script>
 
 <template>
@@ -55,29 +16,7 @@ async function fetchManufacturers() {
       <h1 class="text-green-400 text-3xl mb-5">Test FastApi</h1>
 
       <The_Test_DB_countries :url="backend_url"/>
-
-      <div class="grid grid-cols-3 gap-2">
-        <button class="btn btn-p" @click="fetchManufacturers">Get Manufacturers from DB</button>
-        <div v-if="all_manufacturers && all_manufacturers.length > 0">
-          <table>
-            <thead>
-            <tr>
-              <th>id</th>
-              <th>name</th>
-              <th>country_id</th>
-            </tr>
-            </thead>
-            <tr v-for="manufacturer in all_manufacturers" :key="manufacturer.id">
-              <td>{{ manufacturer.id }}</td>
-              <td>{{ manufacturer.name }}</td>
-              <td v-if="'country_id' in manufacturer">{{ manufacturer.country_id }}</td>
-              <td v-else>N/A</td>
-            </tr>
-          </table>
-        </div>
-        <p class="text-red-400" v-if="all_manufacturers_error">{{ all_manufacturers_error }}</p>
-      </div>
-      <hr class="mb-5">
+      <The_Test_DB_manufacturers :url="backend_url"/>
 
     </div>
   </div>
