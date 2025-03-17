79c996e63d77f11093ef6aa0d97d112b57873f6f
Maksim <voronovmaksim88@gmail.com>
2025-03-17 11:13:37 +0700

    мелкие правки

--- backend/utils/import_data.py
+++ backend/utils/import_data.py
@@ -13,15 +13,32 @@ from typing import Dict, Set, Any
 # Добавляем родительскую директорию в путь поиска модулей
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-# Импорты, требующие модификации sys.path
-from kis2.DjangoRestAPI import create_countries_set_from_kis2, create_companies_list_dict_from_kis2, \
-    create_list_dict_manufacturers, create_equipment_type_set_from_kis2, create_money_set_from_kis2, \
-    create_cities_set_from_kis2, create_companies_form_from_kis2, create_person_list_dict_from_kis2, \
-    create_works_list_dict_from_kis2
+# Игнорирование ошибки PEP 8: E402 для импортов, требующих модификации sys.path
+# pylint: disable=E402
+from kis2.DjangoRestAPI import create_countries_set_from_kis2
+from kis2.DjangoRestAPI import create_companies_list_dict_from_kis2
+from kis2.DjangoRestAPI import create_list_dict_manufacturers
+from kis2.DjangoRestAPI import create_equipment_type_set_from_kis2
+from kis2.DjangoRestAPI import create_money_set_from_kis2
+from kis2.DjangoRestAPI import create_cities_set_from_kis2
+from kis2.DjangoRestAPI import create_companies_form_from_kis2
+from kis2.DjangoRestAPI import create_person_list_dict_from_kis2
+from kis2.DjangoRestAPI import create_works_list_dict_from_kis2
 from kis2.DjangoRestAPI import create_orders_list_dict_from_kis2
+
 from database import SyncSession, test_sync_connection
-from models.models import Country, Manufacturer, Counterparty, CounterpartyForm, City, OrderStatus, \
-    EquipmentType, Currency, Person, Work, Order
+from models.models import Country
+from models.models import Manufacturer
+from models.models import Counterparty
+from models.models import CounterpartyForm
+from models.models import City
+from models.models import OrderStatus
+from models.models import EquipmentType
+from models.models import Currency
+from models.models import Person
+from models.models import Work
+from models.models import Order
+# pylint: enable=E402
 
 # Инициализируем colorama
 init(autoreset=True)
