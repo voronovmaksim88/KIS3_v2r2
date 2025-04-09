039410ba13cb448beba885d905ee433fa2468601
MaksimHome <voronovmaksim88@gmail.com>
2025-04-09 09:14:45 +0700

    сделал поля для доп инфы по заказу

--- frontend/src/components/TheOrders.vue
+++ frontend/src/components/TheOrders.vue
@@ -1,6 +1,6 @@
 // src/components/TheOrders.vue
 <script setup lang="ts">
-import {onMounted} from 'vue';
+import {onMounted, ref} from 'vue';
 import {storeToRefs} from 'pinia';
 import {useOrdersStore} from '../stores/storeOrders';
 import BaseButton from "@/components/Buttons/BaseButton.vue"; // Убедитесь, что путь к стору правильный
@@ -54,12 +54,18 @@ function findOrders() {
 
 }
 
-const showOrderNumber = (serial: string) => {
-  alert(`номер заказа ${serial}`);
+// для хранения серийного номера заказа, чья дополнительная строка должна быть показана.
+const expandedOrderSerial = ref<string | null>(null);
+
+
+const toggleOrderDetails = (serial: string) => {
+  if (expandedOrderSerial.value === serial) {
+    expandedOrderSerial.value = null;
+  } else {
+    expandedOrderSerial.value = serial;
+  }
 };
-
 </script>
-
 <template>
   <div class="w-full min-h-screen flex flex-col items-center bg-gray-800 p-4 text-white">
 
@@ -93,7 +99,7 @@ const showOrderNumber = (serial: string) => {
         </colgroup>
         <thead>
         <tr>
-          <th colspan="7" class="px-2 py-2 text-center bg-gray-600 ">
+          <th colspan="6" class="px-2 py-2 text-center bg-gray-600 ">
             <div class="px-1 py-1 bg-gray-600 flex justify-end items-center">
               <BaseButton
                   :action="addNewOrder"
@@ -121,41 +127,67 @@ const showOrderNumber = (serial: string) => {
         </tr>
         </thead>
         <tbody>
-        <tr v-for="order in orders" :key="order.serial" class="border-t border-gray-600">
-          <td
-              class="px-4 py-2 cursor-pointer hover:bg-gray-600 transition duration-300"
-              :class="{
-              'font-bold': [1, 2, 3, 4, 8].includes(order.status_id),
-              'text-yellow-400': order.status_id === 1,
-              'text-blue-400': order.status_id === 2,
-              'text-green-400': order.status_id === 3,
-              'text-red-400': order.status_id === 4
-              }"
-              @click="showOrderNumber(order.serial)"
-          >
-            {{ order.serial }}
-          </td>
-          <td class="px-4 py-2">{{ order.customer }}</td>
-          <td class="px-4 py-2">{{ order.priority ?? '-' }}</td>
-          <td class="px-4 py-2">{{ order.name }}</td>
-          <td class="px-4 py-2">
-            <p v-for="work in order.works" :key="work.id">
-              • {{ work.name }}
-            </p>
-          </td>
-          <td
-              class="px-4 py-2"
-              :class="{
-              'font-bold': [1, 2, 3, 4, 8].includes(order.status_id),
-              'text-yellow-400': order.status_id === 1,
-              'text-blue-400': order.status_id === 2,
-              'text-green-400': order.status_id === 3,
-              'text-red-400': order.status_id === 4
-              }"
-          >
-            {{ ordersStore.getStatusText(order.status_id) }}
-          </td>
-        </tr>
+        <template v-for="order in orders" :key="order.serial">
+          <tr class="border-t border-gray-600">
+            <td
+                class="px-4 py-2 cursor-pointer hover:bg-gray-600 transition duration-300"
+                :class="{
+                'font-bold': [1, 2, 3, 4, 8].includes(order.status_id),
+                'text-yellow-400': order.status_id === 1,
+                'text-blue-400': order.status_id === 2,
+                'text-green-400': order.status_id === 3,
+                'text-red-400': order.status_id === 4
+                }"
+                @click="toggleOrderDetails(order.serial)"
+            >
+              {{ order.serial }}
+            </td>
+            <td class="px-4 py-2">{{ order.customer }}</td>
+            <td class="px-4 py-2">{{ order.priority ?? '-' }}</td>
+            <td class="px-4 py-2">{{ order.name }}</td>
+            <td class="px-4 py-2">
+              <p v-for="work in order.works" :key="work.id">
+                • {{ work.name }}
+              </p>
+            </td>
+            <td
+                class="px-4 py-2"
+                :class="{
+                'font-bold': [1, 2, 3, 4, 8].includes(order.status_id),
+                'text-yellow-400': order.status_id === 1,
+                'text-blue-400': order.status_id === 2,
+                'text-green-400': order.status_id === 3,
+                'text-red-400': order.status_id === 4
+                }"
+            >
+              {{ ordersStore.getStatusText(order.status_id) }}
+            </td>
+          </tr>
+          <tr v-if="expandedOrderSerial === order.serial" class="border-b border-gray-600">
+            <td colspan="6" class="p-4 bg-gray-700 text-gray-300">
+              Дополнительная информация о заказе {{ order.serial }}.
+              <div style="display: grid; grid-template-columns: repeat(3, 1fr); gap: 10px;">
+                <div class="border rounded-md px-1">
+                  <p>коменты</p>
+                </div>
+
+                <div style="display: grid; grid-template-rows: repeat(2, auto); gap: 10px;">
+                  <div class="border rounded-md px-1">
+                    <p>время</p>
+                  </div>
+
+                  <div class="border rounded-md px-1">
+                    <p>деньги</p>
+                  </div>
+                </div>
+
+                <div class="border rounded-md px-1">
+                  <p>задачи</p>
+                </div>
+              </div>
+            </td>
+          </tr>
+        </template>
         </tbody>
       </table>
 
